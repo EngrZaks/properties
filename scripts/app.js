f0e07a7fd073a8be8a538d729c557e09146a2643
DevZaks <abzakariyya@gmail.com>
2021-11-13 04:40:24 +0100

    added SEO and made it PWA

--- scripts/app.js
+++ scripts/app.js
@@ -186,3 +186,11 @@ navList.forEach((element) => {
 mainNavList.forEach((element) => {
   element.addEventListener("click", handleMainNavigate);
 });
+
+//regieter serviceWorker
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then((reg) => console.log("serviceWorker registered"))
+    .catch((err) => console.log("unable to register serviceWorker", err));
+}
---
+++ scripts/sw.js
@@ -0,0 +1,63 @@
+const staticCacheName = "site-static-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/assets",
+  "/assets/arigo.jpg",
+  "/assets/bambara.jpg",
+  "/assets/close.svg",
+  "/assets/logo.svg",
+  "/assets/menu.svg",
+  "/assets/search.svg",
+  "/assets/barbounia-beans.jpg",
+  "/assets/cocoabeans.jpg",
+  "/assets/corn.jpg",
+  "/assets/cowpea.jpg",
+  "/assets/cumin.jpg",
+  "/assets/grahm.jpg",
+  "/assets/groundnut.jpg",
+  "/assets/kidneybeans.jpg",
+  "/assets/millet.jpg",
+  "/assets/rice.jpg",
+  "/assets/sorghums.jpg",
+  "/assets/soybean.jpg",
+  "/assets/sonflower-seeds.jpg",
+  "/assets/wheat-grains.jpg",
+  "/scripts",
+  "/scripts/properties.js",
+  "/scripts/helpers.js",
+  "/scripts/app.js",
+  "/styles",
+  "/styles/styles.css",
+  "https://fonts.googleapis.com/css2?family=Gluten:wght@300&family=Roboto&family=Roboto+Condensed&family=Roboto+Slab:wght@700&display=swap",
+];
+// install event
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log("caching shell assets");
+      cache.addAll(assets);
+    })
+  );
+});
+// activate event
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== staticCacheName)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+// When we change the name we could have multiple cache, to avoid that we need to delet the old cache, so with this function we check the key that is our cache naming, if it is different from the actual naming we delete it, in this way we will always have only the last updated cache.
+// fetch event
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((cacheRes) => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});
